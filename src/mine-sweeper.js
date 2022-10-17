2cd145fb1e010a1fe576a2a6b91811cecabdea0b
Zhuk1305 <vaneczhuk@gmail.com>
2022-10-17 14:20:54 +0300

    feat: add mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  const resMatr = [];
+  function setMine(i, j) {
+    let count = 0;
+
+    if (i === 0) {
+      matrix[i + 1][j] && count++;
+      if (j === 0) {
+        matrix[i + 1][j + 1] && count++;
+        matrix[i][j + 1] && count++;
+      } else if (j === matrix[i].length - 1) {
+        matrix[i + 1][j - 1] && count++;
+        matrix[i][j - 1] && count++;
+      } else {
+        matrix[i + 1][j + 1] && count++;
+        matrix[i + 1][j - 1] && count++;
+        matrix[i][j + 1] && count++;
+        matrix[i][j - 1] && count++;
+      }
+    } else if (i === matrix.length - 1) {
+      matrix[i - 1][j] && count++;
+      if (j === 0) {
+        matrix[i - 1][j + 1] && count++;
+        matrix[i][j + 1] && count++;
+      } else if (j === matrix[i].length - 1) {
+        matrix[i - 1][j - 1] && count++;
+        matrix[i][j - 1] && count++;
+      } else {
+        matrix[i - 1][j + 1] && count++;
+        matrix[i - 1][j - 1] && count++;
+        matrix[i][j + 1] && count++;
+        matrix[i][j - 1] && count++;
+      }
+    } else {
+      matrix[i - 1][j] && count++;
+      matrix[i + 1][j] && count++;
+      if (j === 0) {
+        matrix[i + 1][j + 1] && count++;
+        matrix[i - 1][j + 1] && count++;
+        matrix[i][j + 1] && count++;
+      } else if (j === matrix[i].length - 1) {
+        matrix[i - 1][j - 1] && count++;
+        matrix[i + 1][j - 1] && count++;
+        matrix[i][j - 1] && count++;
+      } else {
+        matrix[i - 1][j + 1] && count++;
+        matrix[i - 1][j - 1] && count++;
+        matrix[i + 1][j + 1] && count++;
+        matrix[i + 1][j - 1] && count++;
+        matrix[i][j + 1] && count++;
+        matrix[i][j - 1] && count++;
+      }
+    }
+
+    return count;
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    resMatr[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      resMatr[i][j] = setMine(i, j);
+    }
+  }
+  return resMatr;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
